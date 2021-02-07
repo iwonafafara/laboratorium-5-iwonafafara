dbd3c2f33412e53b72798313af420bcdf3b7f1e0
iwonafafara <67429916+iwonafafara@users.noreply.github.com>
2021-02-07 14:29:20 +0100

    workspace

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import scipy
+import pickle
+
+#from typing import Union, List, Tuple
+
+
+def first_spline(x: np.ndarray, y: np.ndarray):
+    """Funkcja wyznaczająca wartości współczynników spline pierwszego stopnia.
+
+    Parametrs:
+    x(float): argumenty, dla danych punktów
+    y(float): wartości funkcji dla danych argumentów
+
+    return (a,b) - krotka zawierająca współczynniki funkcji linowych"""
+
+    return None
+
+
+def cubic_spline(x: np.ndarray, y: np.ndarray):
+    """Funkcja wyznaczająca wartości współczynników spline trzeciego stopnia.
+
+    Parametrs:
+    x(float): argumenty, dla danych punktów
+    y(float): wartości funkcji dla danych argumentów
+
+    Results:
+    (a0,a1,a2,a3) - krotka zawierająca współczynniki funkcji wielomianowych"""
+
+    return None
+
+
